e1a3ff3d01f2ad543633df69bf345157087ac6a6
ssouf849 <ouattaraissouf849@gmail.com>
2026-04-20 15:34:45 +0000

    Fix MIME type error avec base './'

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mon_site/',  // 👈 C'est la ligne importante !
+  base: './',  // 👈 Utiliser './' au lieu de '/mon_site/'
 })
