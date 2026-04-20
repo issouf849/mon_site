69551cb873f73565c1bc95a6552d02db84bda717
ssouf849 <ouattaraissouf849@gmail.com>
2026-04-20 15:27:41 +0000

    Ajout de la base path pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mon_site/", // ⚠️ nom EXACT du repo GitHub
+  base: '/mon_site/',  // 👈 C'est la ligne importante !
 })
